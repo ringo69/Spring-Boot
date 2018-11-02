041ec73b018418102be143ca69463d43e1356130
nikrin <niklas.ringstam@lm.se>
2018-11-02 09:26:03 +0100

    fix

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -5,9 +5,9 @@ import java.util.Arrays;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
