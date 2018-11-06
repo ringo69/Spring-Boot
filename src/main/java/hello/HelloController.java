6401224ce2c370c9dff5c116e5d58907d2a93f22
nikrin <niklas.ringstam@lm.se>
2018-11-06 10:02:51 +0100

    uppdaterat

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,17 +1,18 @@
 package hello;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
 
-    /*@Value("${greeting.message}")
-    String message;*/
+    @Value("${greeting.message}")
+    String message;
 
     @RequestMapping("/")
     public String index() {
-        return System.getenv().getOrDefault("GREETINGS_MESSAGE", "Hello");
+        return message;
     }
 
 }
